e8ce7b78800ff15c0f0a049b25b05f218b521e57
Cat Gomez <catherinedgomez@gmail.com>
2020-07-30 02:33:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,9 @@
 class Book
-  attr_accessor :title, :author, :page_count, :genre
-
-  def initialize(title, author, page_count, genre)
-    @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
-  end
-
+  
+   def initialize(title)
+   end
+  
+   def title
+   end
+  
 end
